d70b12136defa0e7ee264b0c79be15076a0ab8df
Zidan-09 <samuel.profissional18@gmail.com>
2025-04-01 11:36:05 -0300

    Atividade sobre listas

---
+++ ListasLigadasCirculares.py
@@ -0,0 +1,51 @@
+class Soldado:
+    def __init__(self, id, nome):
+        self.id = id
+        self.nome = nome
+
+    def __str__(self):
+        return f'{self.nome}'
+
+class No:
+    def __init__(self, soldado, ponteiro):
+        self.soldado = soldado
+        self.ponteiro = ponteiro
+
+    def __repr__(self):
+        return f'{self.soldado}'
+
+qty_soldados = int(input('Quantidade de soldados:'))
+
+temp_soldado = Soldado(int(input('id:')), input('Nome:'))
+temp_no = No(temp_soldado, None)
+
+primeiro = [temp_no]
+
+for i in range(qty_soldados - 1):
+
+    soldado = Soldado(int(input('Id:')), input('Nome:'))
+    no = No(soldado, None)
+
+    temp_no.ponteiro = no
+    temp_no = no
+
+temp_no.ponteiro = primeiro[0]
+
+soldado_escolhido = int(input('Qual o soldado (id):'))
+vezes = int(input('Quantas vezes:'))
+
+nos = primeiro[0]
+
+for i in range(qty_soldados):
+    print(nos)
+
+    if nos.soldado.id == soldado_escolhido:
+        for j in range(vezes):
+            nos = nos.ponteiro
+            print(nos)
+
+        print(f'Soldado Escolhido: {nos}')
+        break
+
+    else:
+        nos = nos.ponteiro
